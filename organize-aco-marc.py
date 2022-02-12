abd21d17b237668e4af86f7b50d94aea1e9712b0
Rasan Rasch <rasan@nyu.edu>
2022-02-12 10:11:38 -0500

    Fix xml processing

--- organize-aco-marc.py
+++ organize-aco-marc.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-from pymarc import parse_xml_to_array, map_xml, MARCWriter
+from pymarc import parse_xml_to_array, map_xml, record_to_xml, MARCWriter
+
+from xml.etree import ElementTree as ET
+
+from pathlib import Path
 
 import argparse
 import glob
@@ -14,8 +18,20 @@ from pprint import pprint
 
 partners = ["LeBAU", "NIC", "NNC", "NNU", "NjP", "UaCaAUL", "aeadna"]
 
-marcxml_schema = "http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd"
+MARCXML_SCHEMA = "http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd"
 
+nsmap = {"m": "http://www.loc.gov/MARC21/slim"}
+
+ET.register_namespace('', nsmap["m"])
+
+# https://stackoverflow.com/questions/18159221/remove-namespace-and-prefix-from-xml-in-python-using-lxml
+def remove_namespace(doc, namespace):
+    """Remove namespace in the passed document in place."""
+    ns = u'{%s}' % namespace
+    nsl = len(ns)
+    for elem in doc.iter():
+        if elem.tag.startswith(ns):
+            elem.tag = elem.tag[nsl:]
 
 def find_dirs(name, path):
     result = []
@@ -34,12 +50,22 @@ def get_batch_date(partner, batch):
 def do_cmd(cmdlist, **kwargs):
     cmd = list(map(str, cmdlist))
     logging.debug("Running command: %s", " ".join(cmd))
+    process = None
     try:
         process = subprocess.run(cmd, check=True, **kwargs)
+    except subprocess.CalledProcessError as e:
+        logging.exception(e)
     except Exception as e:
         logging.exception(e)
-        exit(1)
-    return process
+    if process and process.returncode == 0:
+        return True
+    else:
+        return False
+
+
+def validate(xml_file):
+    return do_cmd(['xmllint', '--noout',
+        '--schema', 'MARC21slim.xsd', xml_file])
 
 
 def merge_xml(input_paths, output_path):
@@ -51,17 +77,26 @@ def merge_xml(input_paths, output_path):
     xsi:schemaLocation="http://www.loc.gov/MARC21/slim
         {}"
     xmlns="http://www.loc.gov/MARC21/slim">
-""".format(marcxml_schema).lstrip())
+""".format(MARCXML_SCHEMA).lstrip())
         for input_path in input_paths:
             logging.debug(f"marc xml file: {input_path}")
-            with open(input_path) as infile:
-                lines = infile.readlines()
-                for line in lines[5:-1]:
-                    outfile.write(" " * indent)
-                    outfile.write(line)
+
+            tree = ET.parse(input_path)
+            root = tree.getroot()
+            remove_namespace(root, nsmap['m'])
+            for attr in list(root.attrib):
+                root.attrib.pop(attr)
+
+            if root.tag.endswith('record'):
+                root.tag = "record"
+            else:
+                records = root.findall('./record')
+
+            for record in records:
+                outfile.write(ET.tostring(record, encoding="unicode"))
+
         outfile.write('</collection>')
-    do_cmd(['xmllint', '--noout',
-        '--schema', marcxml_schema, output_path])
+    return validate(output_path)
 
 def save_as_marc(marcxml_files):
     for marcxml_file in marcxml_files:
@@ -73,6 +108,13 @@ def save_as_marc(marcxml_files):
 
 # https://www.oahelper.org/2018/09/06/pymarc-python-marc-record-processing-convert-marcxml-to-marc/
 def xml2mrc(input_xml_file, output_mrc_file=None):
+#     if not os.path.exists(input_xml_file):
+#         logging.error(f"{input_xml_file} does not exist.")
+#         exit(1)
+    try:
+        Path(input_xml_file).resolve(strict=True)
+    except Exception as e:
+        logging.exception(e)
     if not output_mrc_file:
         output_mrc_file = input_xml_file.replace('.xml', '.mrc')
     writer = MARCWriter(open(output_mrc_file, 'wb'))
@@ -89,6 +131,9 @@ parser.add_argument("-o", "--output-dir", metavar="OUTPUT_DIRECTORY",
     help="Output directory")
 parser.add_argument("-d", "--debug",
     help="Enable debugging messages", action="store_true")
+parser.add_argument("-v", "--validate",
+    help="Validate marcxml files in marcxml_out direcory",
+    action="store_true")
 args = parser.parse_args()
 
 if args.debug:
@@ -97,14 +142,20 @@ if args.debug:
 marc_dirs = find_dirs("marcxml_out", args.work_dir)
 
 # pprint(marc_dirs)
-
 partner_pos = len(args.work_dir) + 1
+
+partner_files = dict.fromkeys(partners, [])
+
+all_files = []
+
+if args.validate:
+    invalid = open('invalid.txt', 'w')
 
 for marc_dir in marc_dirs:
     logging.debug(f"marcxml_out dir: {marc_dir}")
     
     marc_dir_rel = marc_dir[partner_pos:]
-    print(f"relative marcxml_out dir: {marc_dir_rel}")
+    logging.debug(f"relative marcxml_out dir: {marc_dir_rel}")
 
     partner, batch, marcout = marc_dir_rel.split(os.sep)
     logging.debug(f"partner {partner}")
@@ -112,24 +163,37 @@ for marc_dir in marc_dirs:
     batch_date = get_batch_date(partner, batch)
     logging.debug(f"batch date: {batch_date}")
 
-    marc_files = sorted(glob.glob(f"{marc_dir}/*_marcxml.xml"))
+    batch_files = sorted(glob.glob(f"{marc_dir}/*_marcxml.xml"))
 
-    out_xml = os.path.join(args.output_dir, "out.xml")
+    for file in batch_files:
+        if args.validate:
+            if validate(file):
+                partner_files[partner].append(file)
+                all_files.append(file)
+            else:
+                invalid.write(file + '\n')
+        else:
+            partner_files[partner].append(file)
+            all_files.append(file)
+
+if args.validate:
+    invalid.close()
+
+for partner in partners:
+
+    out_xml = os.path.join(args.output_dir, f"{partner}.xml")
     logging.debug(out_xml)
 
-    merge_xml(marc_files, out_xml)
+    logging.debug(f"merging partner {partner} files to {out_xml}")
+    merge_xml(partner_files[partner], out_xml)
 
+    logging.debug(f"converting {out_xml} to mrc format")
     xml2mrc(out_xml)
 
+out_xml = os.path.join(args.output_dir, "all.xml")
+logging.debug(f"merging all files to {out_xml})")
+merge_xml(all_files, out_xml)
 
-    #save_as_marc(marc_files)
-
-
-#     for marc_file in marc_files:
-#         record = parse_xml_to_array(marc_file)[0]   
-# 
-#     print(marcxml.record_to_xml(record))
-
-    exit(1)
-
+logging.debug(f"converting {out_xml} to mrc format")
+xml2mrc(out_xml)
 
